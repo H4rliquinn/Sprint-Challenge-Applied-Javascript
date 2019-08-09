ed389d438ff5ac22e266d21b8172dd1c5de6394a
Jason Sonnichsen <Harliquinn@gmail.com>
2019-08-09 12:34:59 -0400

    MVP Reached

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,10 +17,48 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+const cardContainer=document.querySelector('.cards-container');
+
 axios.get('https://lambda-times-backend.herokuapp.com/articles')
 .then((res)=>{
-  console.log(res.data);
+  console.log(res.data.articles);
+  const topics=Object.keys(res.data.articles);
+//   console.log(topics);
+  topics.forEach(item=>{
+    //   console.log(item);
+        res.data.articles[item].forEach(item=>{
+        cardContainer.appendChild(cardMaker(item));
+    });
+  });
 })
 .catch((err) => {
   console.log(err)
-}); 
+});
+
+function cardMaker(article){
+    const newArticle=document.createElement('div');
+    newArticle.classList.add('card');
+  
+    const newHeadline=document.createElement('div');
+    newHeadline.classList.add('headline');
+    newHeadline.textContent=article.headline;
+    newArticle.appendChild(newHeadline);
+
+    const newAuthor=document.createElement('div');
+    newAuthor.classList.add('author');
+    newArticle.appendChild(newAuthor);
+  
+    const newImgCont=document.createElement('div');
+    newImgCont.classList.add('img-container');
+    newAuthor.appendChild(newImgCont);
+
+    const newImg=document.createElement('img');
+    newImg.src=article.authorPhoto;
+    newImgCont.appendChild(newImg);
+  
+    const newName=document.createElement('span');
+    newName.textContent=article.authorName;
+    newImgCont.appendChild(newName);
+
+    return newArticle;
+  }
