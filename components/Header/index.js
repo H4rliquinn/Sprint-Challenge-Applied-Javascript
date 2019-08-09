9a573bff9e335a86f3fa69e78f360e58b2627692
Jason Sonnichsen <Harliquinn@gmail.com>
2019-08-09 11:42:12 -0400

    Header created

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,31 @@
 //  </div >
 // And add it to the DOM in the .headerContainer component
 
-function Header() {}
+const container=document.querySelector('.header-container');
+container.appendChild(Header());
+
+function Header() {
+const newHeader=document.createElement('div');
+newHeader.classList.add('header');
+  
+  const newDate=document.createElement('span');
+  newDate.classList.add('date');
+//   newDate.textContent=article.date;
+    newDate.textContent='MARCH 28, 2019';
+
+  newHeader.appendChild(newDate);
+
+  const newTitle=document.createElement('h1');
+//   newTitle.textContent=article.header;
+  newTitle.textContent='Lambda Times';
+  newHeader.appendChild(newTitle);
+
+  const newTemp=document.createElement('span');
+  newTemp.classList.add('temp');
+//   newTemp.textContent=article.temp;
+  newTemp.textContent='98°';
+
+  newHeader.appendChild(newTemp);
+
+  return newHeader;
+}
